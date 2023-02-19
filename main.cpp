d2ee921afed317bb79680cc261677493c51d974f
V-Mac <lytranvinh.2509@gmail.com>
2023-02-20 00:03:57 +0700

    v push ...

--- main.cpp
+++ main.cpp
@@ -1,40 +1,52 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-
+struct book{
+    int ma_sach;
+    double gia_tien;
+    string ten_tac_gia, ten_sach;
+};
+double max(double a, double b){
+    return a > b ? a : b;
+}
 int main(){
-    
-    int n;
-    map<string, int> mp;
-    map<string, int> :: iterator it;
+    #ifndef ONLINE_JUDGE
+    freopen("INP.TXT", "r", stdin);
+	freopen("OUT.TXT", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    int n, c = 0;
+    double mx = -1;
+    string ten_tac_gia;
     cin >> n;
-    cin.ignore();
-    for (int i = 0; i < n*(n-1)/2; i++){
-        string str;
-        //cin.clear();
-        
-        getline(cin, str); 
-        int size_str = str.length();
-        for (int j = 0; j < size_str; j++){
-            if(isdigit(str[j])){
-                if (str[j] > str[j + 4]){
-                    mp[str.substr(0, j - 1)] += 3;
-                }
-                else if (str[j] == str[j + 4]){
-                    mp[str.substr(0, j - 1)] ++;
-                    mp[str.substr(j + 6, size_str - j - 6)] ++;
-                }
-                else mp[str.substr(j + 6, size_str - j - 6)] += 3; 
-                
-                break;
-            }
-        } 
+    book Book[n+3];
+    // nhap theo thu tu: ma sach, ten sach, gia tien, tac gia.
+    for (int i = 0; i < n; i++){
+        cin >> Book[i].ma_sach >> Book[i].ten_sach >> Book[i].gia_tien >> Book[i].ten_tac_gia;
+        mx = max(mx, Book[i].gia_tien);
     }
- 
-    for(it = mp.begin();it != mp.end(); it++){
-        cout << it->first << " " << it->second << endl;
-    } 
-    
+    cin >> ten_tac_gia;
+    cout << "Nhung cuon sach cua thoa man la: " << endl;
+    for (int i = 0; i < n; i++){
+        if (ten_tac_gia == Book[i].ten_tac_gia){
+            cout << "   " << Book[i].ten_sach << endl;
+            c++;
+        }
+    }
+    if (!c){
+        cout << "-1" << endl;
+    }
+    cout << "Sach dat nhat la: " << endl;
+    for (int i = 0; i < n; i++){
+        if (mx == Book[i].gia_tien){
+            cout << "   " << Book[i].ten_sach << endl;
+        }
+    }
+    cout << "Sach gia hon 100 la: " << endl;
+    for (int i = 0; i < n; i++){
+        if (Book[i].gia_tien > 100){
+            cout << "   " << Book[i].ten_sach << endl;
+        }
+    }
     return 0;
 }
